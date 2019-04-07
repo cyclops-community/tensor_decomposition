24a2af3746a090da75a1290610d6e664271a4104
Edgar Solomonik <solomon2@illinois.edu>
2019-04-07 14:20:09 -0500

    add runtime paramters and do low rank SVD differently

--- als_lowr_msdt_order3.py
+++ als_lowr_msdt_order3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as la
 import ctf
+import sys
 
 def compute_lin_sys(X, Y):
     return (X.T() @ X) * (Y.T() @ Y)
@@ -53,7 +54,10 @@ def solve_sys_lowr(G, RHS, r):
     S = 1./S
     X = RHS @ U
     0.*X.i("ij") << S.i("j") * X.i("ij")
-    return [X[:,:r], VT[:r,:]]
+    [U,S,VT]=ctf.svd(X@ VT)
+    0.*VT.i("ij") << S.i("i") * VT.i("ij")
+    return [U[:,:r],VT[:r,:]]
+    #return [X[:,-r:], VT[-r:,:]]
 
 
 def lowr_msdt_step(T,A,B,C,RHS_A,RHS_B,RHS_C,r):
@@ -93,32 +97,57 @@ def init_rand(s,R):
     C = ctf.random.random((s,R))
     return [A,B,C,T]
 
-def test_rand_naive(s,R):
+def test_rand_naive(s,R,num_iter):
     [A,B,C,T] = init_rand(s,R)
-    for i in range(10):
+    for i in range(num_iter):
         res = get_residual(T,A,B,C)
         if ctf.comm().rank() == 0:
             print("Residual is", res)
         [A,B,C] = naive_ALS_step(T,A,B,C)
 
-def test_rand_lowr(s,R,r):
+def test_rand_lowr(s,R,r,num_iter,num_lowr_init_iter):
     [A,B,C,T] = init_rand(s,R)
-    for i in range(2):
+    for i in range(num_lowr_init_iter):
         res = get_residual(T,A,B,C)
         if ctf.comm().rank() == 0:
             print("Residual is", res)
         [A,B,C] = naive_ALS_step(T,A,B,C)
     [RHS_A,RHS_B,RHS_C] = build_leaves(T,A,B,C)
-    for i in range(8):
+    for i in range(num_iter-num_lowr_init_iter):
         res = get_residual(T,A,B,C)
         if ctf.comm().rank() == 0:
             print("Residual is", res)
         [A,B,C,RHS_A,RHS_B,RHS_C] = lowr_msdt_step(T,A,B,C,RHS_A,RHS_B,RHS_C,r)
 
 
-s = 40
-R = 10
-r = 10
+if __name__ == "__main__":
+    s = 40
+    R = 10
+    r = 10
+    num_iter = 10
+    num_lowr_init_iter = 2
+    run_naive = 1
+    run_lowr = 1
+    if len(sys.argv) >= 4:
+        s = int(sys.argv[1])
+        R = int(sys.argv[2])
+        r = int(sys.argv[3])
+    if len(sys.argv) >= 5:
+        num_iter = int(sys.argv[4])
+    if len(sys.argv) >= 6:
+        num_lowr_init_iter = int(sys.argv[5])
+    if len(sys.argv) >= 7:
+        run_naive = int(sys.argv[6])
+    if len(sys.argv) >= 8:
+        run_lowr = int(sys.argv[6])
 
-test_rand_naive(s,R)
-test_rand_lowr(s,R,r)
+    if ctf.comm().rank() == 0:
+        print("Arguments to exe are (s,R,r,num_iter,num_lowr_init_iter,run_naive,run_lowr), default is (",40,10,10,10,2,1,1,")provided", sys.argv)
+    if run_naive:
+        if ctf.comm().rank() == 0:
+            print("Testing naive version, printing residual before every ALS sweep")
+        test_rand_naive(s,R,num_iter)
+    if run_lowr:
+        if ctf.comm().rank() == 0:
+            print("Testing low rank version, printing residual before every ALS sweep")
+        test_rand_lowr(s,R,r,num_iter,num_lowr_init_iter)
