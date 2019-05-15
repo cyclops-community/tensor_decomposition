c2726aee6b093f78346615bca92f253e9d3d1acc
LinjianMa <malinjianev@gmail.com>
2019-05-14 21:08:49 -0700

    modify cptest

--- cptests.py
+++ cptests.py
@@ -2,27 +2,52 @@ import unittest
 from CPD.common_kernels import solve_sys
 import tensors.synthetic_tensors as stsrs
 from cpals import CP_ALS
-import ctf
 
 class CPDALSTestCase(unittest.TestCase):
     """Tests for CPD_ALS."""
 
-    def test_random_tensor_dense(self):
-        [T,O] = stsrs.init_rand(order=3,s=10,R=5,sp_frac=1.)
+    def test_random_numpy_dense(self):
+        import numpy_ext as tenpy
+
+        [T,O] = stsrs.init_rand(tenpy,order=3,s=10,R=5,sp_frac=1.)
         A = []
         for i in range(T.ndim):
-            A.append(ctf.random.random((T.shape[i],5)))
+            A.append(tenpy.random((T.shape[i],5)))
 
-        res = CP_ALS(A,T,O,num_iter=30,sp_res=0,csv_writer=None,Regu=1e-6)
+        res = CP_ALS(tenpy,A,T,O,num_iter=30,sp_res=0,csv_writer=None,Regu=1e-6)
         self.assertTrue(res<=0.5)
 
-    def test_random_tensor_sparse(self):
-        [T,O] = stsrs.init_rand(order=3,s=10,R=5,sp_frac=0.99)
+    def test_random_numpy_sparse(self):
+        import numpy_ext as tenpy
+ 
+        [T,O] = stsrs.init_rand(tenpy,order=3,s=10,R=5,sp_frac=0.99)
         A = []
         for i in range(T.ndim):
-            A.append(ctf.random.random((T.shape[i],5)))
+            A.append(tenpy.random((T.shape[i],5)))
 
-        res = CP_ALS(A,T,O,num_iter=30,sp_res=0,csv_writer=None,Regu=1e-6)
+        res = CP_ALS(tenpy,A,T,O,num_iter=30,sp_res=0,csv_writer=None,Regu=1e-6)
+        self.assertTrue(res<=0.5)
+
+    def test_random_ctf_dense(self):
+        import ctf_ext as tenpy
+
+        [T,O] = stsrs.init_rand(tenpy,order=3,s=10,R=5,sp_frac=1.)
+        A = []
+        for i in range(T.ndim):
+            A.append(tenpy.random((T.shape[i],5)))
+
+        res = CP_ALS(tenpy,A,T,O,num_iter=30,sp_res=0,csv_writer=None,Regu=1e-6)
+        self.assertTrue(res<=0.5)
+
+    def test_random_ctf_sparse(self):
+        import ctf_ext as tenpy
+ 
+        [T,O] = stsrs.init_rand(tenpy,order=3,s=10,R=5,sp_frac=0.99)
+        A = []
+        for i in range(T.ndim):
+            A.append(tenpy.random((T.shape[i],5)))
+
+        res = CP_ALS(tenpy,A,T,O,num_iter=30,sp_res=0,csv_writer=None,Regu=1e-6)
         self.assertTrue(res<=0.5)
 
 if __name__ == '__main__':
