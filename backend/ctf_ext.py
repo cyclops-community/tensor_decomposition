1ea82fbf0ad965e60efc494baf7b251d05ed0e20
Tianyi Hao <haoty0116@gmail.com>
2019-07-10 12:50:49 -0500

    add ellipses support for ctf einsum; add ctf einsvd and squeeze

--- backend/ctf_ext.py
+++ backend/ctf_ext.py
@@ -58,6 +58,18 @@ def solve_tri(A, B, lower=True, from_left=False, transp_L=False):
     return ctf.solve_tri(A, B, lower, from_left, transp_L)
 
 def einsum(string, *args):
+    if "..." in string:
+        left = string.split(",")
+        left[-1], right = left[-1].split("->")
+        symbols = "".join(list(set([chr(i) for i in range(48, 127)]) - set(string.replace(".", "").replace(",", "").replace("->", ""))))
+        symbol_idx = 0
+        for i, (s, tsr) in enumerate(zip(left, args)):
+            num_missing = tsr.ndim - len(s.replace("...", ""))
+            left[i] = s.replace("...", symbols[symbol_idx:symbol_idx+num_missing])
+            symbol_idx += num_missing
+        right = right.replace("...", symbols[:symbol_idx])
+        string = ",".join(left) + "->" + right
+
     return ctf.einsum(string, *args)
 
 def ones(shape):
@@ -89,3 +101,20 @@ def argmax(A, axis=0):
 
 def reshape(A,shape,order='F'):
     return ctf.reshape(A,shape,order)
+
+def einsvd(einstr, A, r=None, transpose=True, compute_uv=True, full_matrices=True, mult_sv=False):
+    str_a, str_uv = einstr.split("->")
+    str_u, str_v = str_uv.split(",")
+    U, S, Vh = A.i(str_a).svd(str_u, str_v, rank=r)
+    if not compute_uv:
+        return S
+    if mult_sv:
+        char_i = list(set(str_v) - set(str_a))[0]
+        char_s = list(set(str_a) - set(str_v))[0]
+        Vh = ctf.einsum(char_s + char_i + "," + str_v + "->" + str_v.replace(char_i, char_s), ctf.diag(S), Vh)
+    if not transpose:
+        Vh = Vh.T
+    return U, S, Vh
+
+def squeeze(A):
+    return A.reshape([s for s in A.shape if s != 1])
